3c0ba900af688a3267437711248cffe159c190c9
gaby205 <c14180055@john.petra.ac.id>
2021-12-06 01:52:31 +0700

    ver 0.0.0

---
+++ lib/main.dart
@@ -0,0 +1,273 @@
+// main.dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Information System',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var main_color = Color(0xffdc143c);
+  List _isHovering = [false, false, false, false];
+  List <Widget> appbar_content= [
+    
+  ];
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 1000),
+        child: Container(
+          color: main_color,
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Text(
+                  'Unit Transfusi Darah Surabaya',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onHover: (value) {
+                          setState(() {
+                            _isHovering[0] = value;
+                          });
+                        },
+                        onTap: () {},
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Discover',
+                              style: TextStyle(
+                                color: _isHovering[0]
+                                    ? Colors.red[100]
+                                    : Colors.white,
+                              ),
+                            ),
+
+                            // For showing an underline on hover
+                            Visibility(
+                              maintainAnimation: true,
+                              maintainState: true,
+                              maintainSize: true,
+                              visible: _isHovering[0],
+                              child: Container(
+                                height: 2,
+                                width: 100,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: screenSize.width / 20),
+                      InkWell(
+                        onHover: (value) {
+                          setState(() {
+                            _isHovering[1] = value;
+                          });
+                        },
+                        onTap: () {},
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Contact Us',
+                              style: TextStyle(
+                                color: _isHovering[1]
+                                    ? Colors.red[100]
+                                    : Colors.white,
+                              ),
+                            ),
+
+                            // For showing an underline on hover
+                            Visibility(
+                              maintainAnimation: true,
+                              maintainState: true,
+                              maintainSize: true,
+                              visible: _isHovering[1],
+                              child: Container(
+                                height: 2,
+                                width: 100,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: screenSize.width / 20),
+                      InkWell(
+                        onHover: (value) {
+                          setState(() {
+                            _isHovering[1] = value;
+                          });
+                        },
+                        onTap: () {},
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Contact Us',
+                              style: TextStyle(
+                                color: _isHovering[1]
+                                    ? Colors.red[100]
+                                    : Colors.white,
+                              ),
+                            ),
+
+                            // For showing an underline on hover
+                            Visibility(
+                              maintainAnimation: true,
+                              maintainState: true,
+                              maintainSize: true,
+                              visible: _isHovering[1],
+                              child: Container(
+                                height: 2,
+                                width: 100,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: screenSize.width / 20),
+                      InkWell(
+                        onHover: (value) {
+                          setState(() {
+                            _isHovering[1] = value;
+                          });
+                        },
+                        onTap: () {},
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Contact Us',
+                              style: TextStyle(
+                                color: _isHovering[1]
+                                    ? Colors.red[100]
+                                    : Colors.white,
+                              ),
+                            ),
+
+                            // For showing an underline on hover
+                            Visibility(
+                              maintainAnimation: true,
+                              maintainState: true,
+                              maintainSize: true,
+                              visible: _isHovering[1],
+                              child: Container(
+                                height: 2,
+                                width: 100,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      _isHovering[2] = value;
+                    });
+                  },
+                  onTap: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color:
+                              _isHovering[2] ? Colors.red[100] : Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: screenSize.width / 50,
+                ),
+                InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      _isHovering[3] = value;
+                    });
+                  },
+                  onTap: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          color:
+                              _isHovering[3] ? Colors.red[100] : Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            // image below the top bar
+            child: SizedBox(
+              height: screenSize.height * 0.45,
+              width: screenSize.width,
+              child: Image.asset(
+                'assets/images/cover.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            heightFactor: 1,
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: screenSize.height * 0.40,
+                left: screenSize.width / 5,
+                right: screenSize.width / 5,
+              ),
+              child: Card(// floating quick access bar
+                  // ...
+                  ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
